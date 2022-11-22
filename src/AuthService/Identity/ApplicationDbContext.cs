1ef464e77a27094983fdcf8685725195045f43ec
Mohamed Anis Laghrour <mohamed.laghrour@outlook.com>
2022-11-21 21:23:38 -0500

    AuthService and updated packages

---
+++ src/AuthService/Identity/ApplicationDbContext.cs
@@ -0,0 +1,30 @@
+﻿using CommonLibrary.AspNetCore.Settings;
+using CommonLibrary.ModelBuilders;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace AuthService.Identity;
+
+public class ApplicationDbContext : IdentityDbContext<IdentityUser>
+{
+    private readonly IConfiguration _configuration;
+
+    public ApplicationDbContext(DbContextOptions options,IConfiguration configuration)
+        : base(options)
+    {
+        _configuration = configuration;
+    }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+        ServiceSettings serviceSettings = _configuration.GetSection(nameof(ServiceSettings)).Get<ServiceSettings>() ?? throw new InvalidOperationException("ServiceSettings is null");
+        optionsBuilder.UseNpgsql(serviceSettings.PostgresConnectionString);
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.BuildCommonLibrary();
+        base.OnModelCreating(modelBuilder);
+    }
+}
--- src/AuthService/Program.cs
+++ src/AuthService/Program.cs
@@ -1,4 +1,6 @@
+using AuthService.Identity;
 using CommonLibrary.AspNetCore;
+using Microsoft.AspNetCore.Identity;
 using Serilog;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -8,9 +10,14 @@ var  MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var logger = new LoggerConfiguration().WriteTo.Console();
 builder.Services.AddCommonLibrary(builder.Configuration, builder.Logging, logger , MyAllowSpecificOrigins);
 builder.Services.AddSwaggerGen();
-
+AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
+builder.Services.AddDbContext<ApplicationDbContext>();
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<ApplicationDbContext>();
 var app = builder.Build();
 app.UseCommonLibrary(MyAllowSpecificOrigins);
+
+    
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
